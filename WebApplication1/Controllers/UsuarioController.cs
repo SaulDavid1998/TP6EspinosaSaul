4222c546db678135e68db56ed066fa192f0d3849
SaulDavid1998 <105959698+SaulDavid1998@users.noreply.github.com>
2024-09-09 20:51:02 -0300

    Initial commit

---
+++ WebApplication1/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Buscar(string nombre)
+        {
+            var input = "<h1>" + nombre + "</h1>";
+            return Content(input);
+        }
+    }
+}
